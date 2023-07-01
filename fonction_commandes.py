ba7595c96032a89fb1c7279c71ca16cb8bccfaf0
Baptiste-Bachelier <baptistebach9@gmail.com>
2023-07-01 15:53:34 +0200

    on peut lané les application si elle sont sur le bureau

--- fonction_commandes.py
+++ fonction_commandes.py
@@ -12,54 +12,36 @@ import pyautogui as pa
 import time
 import winreg
 from pathlib import Path
+import os
 import subprocess
 import pyttsx3
 
 # ouverture page web
 def open_web(nom_site=None):
     wb.open(nom_site)
-    
+  
+# permet d'actionner des toches simultanément     
 def act_key(touches=None):
     pa.hotkey(touches)
 
+# recherche net
 def search(sentences):
     wb.open("https://www.google.com")
     time.sleep(1)
     pa.write(sentences)
     pa.press('enter')
-    
+
+# lance une appli    
 def start(words):
-    reg_path = r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
-    reg_key = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, reg_path)
-
-    installed_applications = {}
-
-    for i in range(winreg.QueryInfoKey(reg_key)[0]):
-        subkey_name = winreg.EnumKey(reg_key, i)
-        subkey_path = reg_path + "\\" + subkey_name
-
-        try:
-            subkey = winreg.OpenKey(winreg.HKEY_LOCAL_MACHINE, subkey_path)
-            program_name = winreg.QueryValueEx(subkey, "DisplayName")[0]
-            executable_path = winreg.QueryValueEx(subkey, "InstallLocation")[0]
-
-            if program_name and executable_path:
-                installed_applications[program_name.lower()] = executable_path
-
-        except OSError:
-            pass
-    
-    if words in installed_applications:
-        folder = Path(installed_applications[words])
-        
-        if folder.is_dir():
-            file_list = folder.iterdir()
-        
-        for file in file_list:
-            if  str(file).split(".")[-1] == "exe":
-                print(file)
-                subprocess.Popen(file)
+    desktop_appli_list = {}
+    key = winreg.OpenKey(winreg.HKEY_CURRENT_USER, r"Software\Microsoft\Windows\CurrentVersion\Explorer\Shell Folders")
+    desktop_path = Path(winreg.QueryValueEx(key, "Desktop")[0])
+    for items in desktop_path.iterdir():
+        desktop_appli_list [os.path.basename(items).split(".")[0].lower()] = str(items)
+    winreg.CloseKey(key)
+    if words in desktop_appli_list:
+        subprocess.Popen(desktop_appli_list[words], shell=True) 
     else:
         pyttsx3.init()
-        pyttsx3.speak("L'application n'est pas installé")                       
+        pyttsx3.speak("L'application n'est pas installé")                                                   
         
